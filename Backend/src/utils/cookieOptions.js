3ef9baed8c10649ee18c9964b9d897be3d923d6c
Saikat Bera <berasaikat731@gmail.com>
2025-06-01 15:55:59 +0530

    edit

--- Backend/src/utils/cookieOptions.js
+++ Backend/src/utils/cookieOptions.js
@@ -1,10 +1,9 @@
 const getCookieOptions = () => {
   return {
     httpOnly: true,
-    secure: true,            // only over HTTPS
-    sameSite: "None",        // required for cross-site cookies
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-    path: "/",               // make cookie available throughout the site
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000
   };
 };
 
