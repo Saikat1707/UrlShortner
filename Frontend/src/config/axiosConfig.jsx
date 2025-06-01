35270d807fd6af5ba64506d008bdc52298624861
Saikat Bera <berasaikat731@gmail.com>
2025-06-01 15:12:46 +0530

    edit

--- Frontend/src/config/axiosConfig.jsx
+++ Frontend/src/config/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://url-shortner-ot9g.vercel.app/',
   withCredentials: true,
 });
 
