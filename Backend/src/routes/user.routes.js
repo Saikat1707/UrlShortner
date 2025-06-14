fc5cdbb10df1eb3e83dff12689820f3b89241ad5
Saikat1707 <berasaikat731@gmal.com>
2025-06-14 11:54:47 +0530

    edit

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import authController from '../controller/auth.controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 router.post("/register",authController.createUser)
 router.post("/login",authController.loginUser)
 router.post("/logout",authController.logoutUser)
-router.get('/current',authController.getCurrentUser)
+router.get('/current',authMiddleware,authController.getCurrentUser)
 
 export default router;
