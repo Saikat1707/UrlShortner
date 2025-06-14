f7dc8a7f0df193534f52f072c1a7af88c8c68ef0
Saikat1707 <berasaikat731@gmal.com>
2025-06-14 11:23:05 +0530

    edit

--- Frontend/src/config/axiosConfig.jsx
+++ Frontend/src/config/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://url-shortner-ot9g.vercel.app/',
+  baseURL: 'https://urlshortner-kio0.onrender.com/',
   withCredentials: true,
 });
 
